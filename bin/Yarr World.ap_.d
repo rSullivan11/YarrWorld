edf0b32da2a71119924ca1933f2af186e50f3166
rsullivan11 <rsullivan11@student.cccd.edu>
2016-09-06 16:10:46 -0700

    Finished working in class. App crashed.

--- bin/Yarr World.ap_.d
+++ bin/Yarr World.ap_.d
@@ -1,16 +1,19 @@
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/Yarr World.ap_ \
  : /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/values/strings.xml \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/values/colors.xml \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/values/fr \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/values/fr/strings.xml \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-xhdpi/ic_launcher.png \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-xhdpi/occpirate.png \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-xhdpi/occlogo.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-ldpi/ic_launcher.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-hdpi/ic_launcher.png \
-/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-hdpi/occpirate.png \
-/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-hdpi/occlogo.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/layout/main.xml \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/res/drawable-mdpi/ic_launcher.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-xhdpi/occpirate.png \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-xhdpi/occlogo.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-ldpi/ic_launcher.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-hdpi/ic_launcher.png \
-/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-hdpi/occpirate.png \
-/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-hdpi/occlogo.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/res/drawable-mdpi/ic_launcher.png \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/AndroidManifest.xml \
--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,9 +1,12 @@
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes.dex : \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R$color.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/BuildConfig.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/MainActivity.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R$string.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R$attr.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R$drawable.class \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/MainActivity.java~ \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R.class \
+/home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R$id.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/rsullivan11/cs273/orangecoastcollege/edu/R$layout.class \
 /home/ryanm/Documents/batcave/android/CSA273/YarrWorld/bin/classes/R.java.d \
